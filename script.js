021b33aabe4fed7866eb3fae94dae194075ff904
Brayan Capet <bcapet@bcapet.jechange.ivalley>
2022-08-05 15:52:41 +0200

    yep

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const base_url = 'https://api.themoviedb.org/3/';
 
 const trending_content = document.querySelector('.trending-content');
 const trending_series_content = document.querySelector('.trending-series-content');
+const categories_content = document.querySelector('.categories-content');
+const popular_people_content = document.querySelector('.popular-people-content');
 
 // My list of movie and series
 let myLibrary = [];
@@ -39,8 +41,24 @@ function getPopularSeries(){
     })
 }
 
+function getCategory(){
+    fetch(`${base_url}/genre/movie/list?${api_key}&language=en-US`)
+    .then(res => res.json())
+    .then(data => {
+        showCategoriesCard(data.genres);
+    })
+}
+
+function getPopularPeople(){
+    fetch(`${base_url}person/popular?${api_key}&language=en-US&page=1`)
+    .then(res => res.json())
+    .then(data => showPeople(data.results));
+}
+
 getPopularMovies();
 getPopularSeries();
+getCategory();
+getPopularPeople();
 
 function showCard(results){
     for(let result of results){
@@ -52,7 +70,9 @@ function showCard(results){
                 <img src="https://image.tmdb.org/t/p/w500${result.poster_path}" style="height: 100%; width:100%;object-fit:cover;border-radius:0.375rem;">
             </div>
             <div class="flip-card-back">
-                <p id="title">${result.title}</p>
+                <div class="card-title">
+                    <p id="title">${result.title}</p>
+                </div>
                 <p id="overview">
                     ${result.overview}
                 </p>
@@ -74,14 +94,47 @@ function showSeriesCard(results){
                 <img src="https://image.tmdb.org/t/p/w500${result.poster_path}" style="height: 100%; width:100%;object-fit:cover;border-radius:0.375rem;">
             </div>
             <div class="flip-card-back">
-                <p id="title">${result.name}</p>
-                <p id="overview">
-                    ${result.overview}
-                </p>
+                <div class="card-title">
+                    <p id="title">${result.name}</p>
+                    <p id="overview">
+                        ${result.overview}
+                    </p>
+                </div>
             </div>
         </div>
         </div>
         `;
         trending_series_content.append(div);
+    }
+}
+
+function showCategoriesCard(genres) {
+    for(let genre of genres) {
+        let div = document.createElement('div');
+        div.classList.add('category-item');
+        div.innerHTML = `
+        <div class="card">
+            <p>${genre.name}</p>
+        </div>
+        `;
+        categories_content.appendChild(div);
+    }
+}
+
+function showPeople(results) {
+    for(let result of results) {
+        let div = document.createElement('div');
+        div.classList.add('people-card');
+        div.innerHTML = `
+        <div class="people-card-content">
+        <div class="people-card-image">
+            <img src="https://image.tmdb.org/t/p/w500${result.profile_path}" alt="Picture of ${result.name}">
+        </div>
+        <div class="people-card-name">
+            <p>${result.name}</p>
+        </div>
+        </div>
+        `;
+        popular_people_content.appendChild(div);
     }
 }
